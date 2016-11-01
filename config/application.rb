8dd408a96f14978d010bc52211a4797ed25c0d92
rogerykyu <rogerykyu@gmail.com>
2016-11-01 13:17:57 +0000

    New Params

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module T
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Hong Kong"
   end
 end
